0d813f33ab40d18c70ade1e5d05dffc0405d1763
fouad mesrane <fouadmesrane@gmail.com>
2025-02-28 08:21:12 -0700

    setting up project and dependecies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+const BASE_URL = import.meta.env.BASE_URL
+const API_KEY = import.meta.env.API_KEY
+function App() {
+  console.log(BASE_URL)
+  console.log(API_KEY)
+
+  return (
+    <>
+     hi
+    </>
+  )
+}
+
+export default App
