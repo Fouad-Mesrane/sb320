f843738503650e79478dab3702c66cddd080a7a6
fouad mesrane <fouadmesrane@gmail.com>
2025-03-03 14:46:22 -0700

    fixed a bug

--- src/pages/TrackingPage.jsx
+++ src/pages/TrackingPage.jsx
@@ -17,6 +17,7 @@ const TrackingPage = () => {
             className="bg-white shadow-md rounded-lg p-4 mb-4 mx-auto text-center w-1/2"
           >
             <h3 className="text-lg">{coinId}</h3>
+            
           </div>
         ))
       ) : (
